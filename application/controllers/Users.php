40ddcbd065379568e61206acce98286324e93f65
GiB07 <ahgeb15@gmail.com>
2026-05-28 15:08:35 +0800

    sda

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -137,7 +137,7 @@ public function login(){
     }
 
     public function register(){
-        $this->load->view('register');
+        $this->load->view('users/register');
     }
 
     public function insert_registration(){
