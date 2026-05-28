0147ecc8964a60f3d13d8478cfc70ca665c47953
GiB07 <ahgeb15@gmail.com>
2026-05-28 14:31:48 +0800

    qwef

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -166,7 +166,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         }
 </style>
 <div class="app">
-    <div class="bg"></div>
+    <div class="bg">
     <form  method="POST" action="<?php echo base_url(); ?>users/login">
     <label style="color: #54546b; font-family:Monotype Corsiva; font-size: 30px;">Get Inked! Tattoo Shop</label><br>
     <label style="color: #54546b; font-size: 15px;"></label>
@@ -210,6 +210,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
             </div>
         </div>
     </form>
+    </div>
 </div>
 <script>
     $(document).ready(function(){
