305ddb916eb6231d54c8db71594722cdb3d67c19
GiB07 <ahgeb15@gmail.com>
2026-06-01 14:30:28 +0800

    ewwq

--- application/views/user_template/navbar.php
+++ application/views/user_template/navbar.php
@@ -1,7 +1,7 @@
 
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
  <link href="<?php echo base_url('assets/css/navbar.css'); ?>" rel="stylesheet">
-<body class="">
+<body class="skin-default-dark fixed-layout">
     <div class="preloader">
         <div class="loader">
             <div class="loader__figure"></div>
