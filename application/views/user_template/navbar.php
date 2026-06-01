5324a17838e8629f0bd04808cbad5d7609fc9dac
GiB07 <ahgeb15@gmail.com>
2026-06-01 16:56:28 +0800

    fas

--- application/views/user_template/navbar.php
+++ application/views/user_template/navbar.php
@@ -18,7 +18,7 @@
             <a class="navbar-brand" href="<?= base_url('users/dashboard'); ?>">
 
                 <div class="brand-logo">
-                    <i class="fa fa-airplane"></i>
+                    <i class="fas fa-airplane"></i>
                 </div>
 
                 <div class="brand-text">
