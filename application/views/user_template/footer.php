6dda5e3f2cb8d1c34ddd3ee4c059e3e8ad053616
GiB07 <ahgeb15@gmail.com>
2026-06-01 11:37:18 +0800

    aaa

--- application/views/user_template/footer.php
+++ application/views/user_template/footer.php
@@ -5,19 +5,7 @@
     
 
     <!-- <script src="<?php echo base_url(); ?>assets/dist/js/jquery.min.js"></script> -->
-    <script src="<?php echo base_url(); ?>assets/node_modules/jquery/jquery-3.2.1.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/dist/js/jquery.dataTables.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/node_modules/popper/popper.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/dist/js/perfect-scrollbar.jquery.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/dist/js/waves.js"></script>
-    <script src="<?php echo base_url(); ?>assets/dist/js/sidebarmenu.js"></script>
-    <script src="<?php echo base_url(); ?>assets/dist/js/custom.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/node_modules/raphael/raphael-min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/node_modules/morrisjs/morris.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/node_modules/jquery-sparkline/jquery.sparkline.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/node_modules/d3/d3.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/node_modules/c3-master/c3.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
     <!-- <script src="<?php echo base_url(); ?>assets/dist/js/jquery-1.11.1.min.js"></script> -->
     <script src="<?php echo base_url(); ?>assets/js/moment.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/fullcalendar.js"></script>
