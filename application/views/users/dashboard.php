dcd43227797d2ba2afcc9c10e83f23c31b71a5ee
GiB07 <ahgeb15@gmail.com>
2026-06-08 15:22:50 +0800

    daa

--- application/views/users/dashboard.php
+++ application/views/users/dashboard.php
@@ -118,7 +118,7 @@
         
         <div class="qty-box">
             <button type="button" class="qty-btn minus">−</button>
-            <input type="number" id="cartQty" value="1" min="1">
+            <input id="cartQty" value="1" min="1">
             <button type="button" class="qty-btn plus">+</button>
         </div>
 
