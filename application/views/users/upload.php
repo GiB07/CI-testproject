85bec7f19753047a4ae59576a8564f0ed199bc36
GiB07 <ahgeb15@gmail.com>
2026-06-11 14:00:49 +0800

    dcz

--- application/views/users/upload.php
+++ application/views/users/upload.php
@@ -51,7 +51,7 @@
                 <div class="d-flex justify-content-between align-items-center mb-4">
                     <h4 class="text-white mb-0">Brewed with Love.</h4>
 
-                    <button class="btn btn-primary"
+                    <button class="btn btn"
                             style="background-color: #d3a05f; border-color: #d3a05f;"
                             data-bs-toggle="modal"
                             data-bs-target="#productModal">
