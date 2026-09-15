392fd4e1ee26dfca0c9622f3792883a83f930c31
GiB07 <ahgeb15@gmail.com>
2026-09-15 17:24:31 +0800

    dsa

--- application/views/users/cart.php
+++ application/views/users/cart.php
@@ -53,37 +53,14 @@
                             foreach ($orders as $value):
 
                                 echo '<tr>';
-
                                 echo '<td class="text-center">'.$count.'</td>';
-
-                                echo '<td class="product" style="text-align:center;">'
-                                    .htmlspecialchars($value->name).
-                                    '</td>';
-
-                                echo '<td class="price" style="text-align:center;">₱'
-                                    .number_format($value->price, 2).
-                                    '</td>';
-
-                                echo '<td class="qty" style="text-align:center;">'
-                                    .$value->qty.
-                                    '</td>';
-
-                                echo '<td class="total_amount" style="text-align:center;">₱'
-                                    .number_format($value->total_amount, 2).
-                                    '</td>';
-
-                                echo '<td class="added_by" style="text-align:center;">'
-                                    .htmlspecialchars($value->added_by).
-                                    '</td>';
-
-                                echo '<td class="created_at" style="text-align:center;">'
-                                    .date('M d, Y h:i A', strtotime($value->created_at)).
-                                    '</td>';
-
-                                echo '<td class="status" style="text-align:center;">'
-                                    .$value->status.
-                                    '</td>';
-
+                                echo '<td class="product" style="text-align:center;">'.htmlspecialchars($value->name).'</td>';
+                                echo '<td class="price" style="text-align:center;">₱'.number_format($value->price, 2).'</td>';
+                                echo '<td class="qty" style="text-align:center;">'.$value->qty.'</td>';
+                                echo '<td class="total_amount" style="text-align:center;">₱'.number_format($value->total_amount, 2).'</td>';
+                                echo '<td class="added_by" style="text-align:center;">'.htmlspecialchars($value->added_by).'</td>';
+                                echo '<td class="created_at" style="text-align:center;">'.date('M d, Y h:i A', strtotime($value->created_at)).'</td>';
+                                echo '<td class="status" style="text-align:center;">'.$value->status.'</td>';
                                 echo '</tr>';
 
                                 $count++;
