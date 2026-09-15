a98e767de7573d75072c44eeb181bb60888be9a6
GiB07 <ahgeb15@gmail.com>
2026-09-15 17:20:43 +0800

    aaa

--- application/views/user_template/footer.php
+++ application/views/user_template/footer.php
@@ -4,12 +4,26 @@
     </div>
     
 
- <script src="<?php echo base_url('assets/js/dataTables.min.js'); ?>"></script>
+    <!-- <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script> -->
+    <script src="<?php echo base_url('assets/bootstrap5/js/bootstrap.bundle.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/dataTables.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/navbar.js'); ?>"></script>
+    <!-- <script src="<?php echo base_url('assets/js/jquery-4.0.0.min.js'); ?>"></script> -->
+    <script src="<?php echo base_url('assets/js/sweetalert.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/moment.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/fullcalendar.js'); ?>"></script>
     <!-- <script src="dist/js/dashboard1.js"></script> -->
-<script>
-$(document).ready(function() {
-    $('#cartTable').DataTable();
-});
+    <script type="text/javascript">
+    $(document).ready(function() {
+
+        $("#cartTable").DataTable({
+
+            order: [[ 0, 'asc' ]],
+            lengthMenu: [ [8, 25, 50, -1], [8, 25, 50, "All"] ]
+
+        });
+
+    });
 </script>
 
 </body>
