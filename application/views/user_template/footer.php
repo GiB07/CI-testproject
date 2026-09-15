c48ee3d8ba5c0f7426f6b10c7d929e7bc8b0121a
GiB07 <ahgeb15@gmail.com>
2026-09-15 17:19:43 +0800

    dasda

--- application/views/user_template/footer.php
+++ application/views/user_template/footer.php
@@ -4,14 +4,7 @@
     </div>
     
 
-    <!-- <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script> -->
-    <script src="<?php echo base_url('assets/bootstrap5/js/bootstrap.bundle.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/dataTables.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/navbar.js'); ?>"></script>
-    <!-- <script src="<?php echo base_url('assets/js/jquery-4.0.0.min.js'); ?>"></script> -->
-    <script src="<?php echo base_url('assets/js/sweetalert.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/moment.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/fullcalendar.js'); ?>"></script>
+ <script src="<?php echo base_url('assets/js/dataTables.min.js'); ?>"></script>
     <!-- <script src="dist/js/dashboard1.js"></script> -->
 <script>
 $(document).ready(function() {
