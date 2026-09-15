b885b99600aeaba25a522ccb992d3a2e6f9116bf
GiB07 <ahgeb15@gmail.com>
2026-09-15 10:43:13 +0800

    ggg

--- application/views/users/upload.php
+++ application/views/users/upload.php
@@ -39,7 +39,7 @@
                         </li>
 
                         <li class="breadcrumb-item active" aria-current="page">
-                            <a class="btn btn-md glass-btn disabled">
+                            <a class="btn btn-md glass-btn disabled" style="font-color: #ffffff; cursor: default;">
                                 Upload Stocks
                             </a>
                         </li>
