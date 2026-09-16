f9ef240219a30d2d52513d876d9d996fa86ccf05
GiB07 <ahgeb15@gmail.com>
2026-09-16 10:14:17 +0800

    2

--- application/views/users/cart.php
+++ application/views/users/cart.php
@@ -92,9 +92,9 @@
                                         echo '<td class="created_at" style="text-align:center;">'.date('M d, Y h:i A', strtotime($value->created_at)).'</td>';
                                         echo '<td class="status" style="text-align:center;">'.$tag.'</td>';
                                         echo '<td style="padding: 3px 3px;text-align:center;">';
-                                        echo '<button type="button" class="ios-action edit" style="padding: 1px 10px; font-size: 14px;" onclick=edit("")><i class="bi bi-pencil" style="color: black;"></i></button>';
+                                        echo '<button type="button" class="ios-action edit" style="padding: 1px 10px; font-size: 14px;" onclick=edit("")><i class="bi bi-pencil"></i></button>';
                                         echo '&nbsp;';
-                                        echo '<button type="button" class="ios-action delete" style="padding: 1px 10px; font-size: 14px;" onclick=remove("")><i class="bi bi-trash" style="color: black;"></i></button>';
+                                        echo '<button type="button" class="ios-action delete" style="padding: 1px 10px; font-size: 14px;" onclick=remove("")><i class="bi bi-trash"></i></button>';
                                         echo '</td>';
                                         echo '</tr>';
 
