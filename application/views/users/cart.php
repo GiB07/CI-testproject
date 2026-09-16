a70f5cadcbb376e8d7981eb9950cd483040745d1
GiB07 <ahgeb15@gmail.com>
2026-09-16 09:30:32 +0800

    7

--- application/views/users/cart.php
+++ application/views/users/cart.php
@@ -83,7 +83,7 @@
                                         echo '<td class="added_by" style="text-align:center;">'.htmlspecialchars($value->added_by).'</td>';
                                         echo '<td class="created_at" style="text-align:center;">'.date('M d, Y h:i A', strtotime($value->created_at)).'</td>';
                                         echo '<td class="status" style="text-align:center;">'.$value->status.'</td>';
-                                        echo '<td style="padding: 3px 3px;">';
+                                        echo '<td style="padding: 3px 3px;text-align:center;">';
                                         echo '<button type="button" class="btn btn-md glass-btn btn-warning" style="padding: 1px 10px; font-size: 14px;" onclick=edit("")><i class="bi bi-pencil" style="color: black;"></i></button>';
                                         echo '&nbsp;';
                                         echo '<button type="button" class="btn btn-md glass-btn btn-danger" style="padding: 1px 10px; font-size: 14px;" onclick=remove("")><i class="bi bi-trash" style="color: black;"></i></button>';
